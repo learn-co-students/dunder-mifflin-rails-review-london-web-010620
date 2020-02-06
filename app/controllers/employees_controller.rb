54cdac17f660f668f4e12e50fafaa7da26c7fd9d
Harriet Bicknell <harrietindubai@gmail.com>
2020-02-06 08:18:18 +0000

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,37 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+    def show
+        @employee = Employee.find(params[:id])
+    end
+    def new
+        @employee = Employee.new
+    end
+    def create
+        @employee = Employee.create(employee_params)
+        # If the pet is valid, redirect to the index.
+        if @employee.valid?
+        redirect_to employees_path
+        # Otherwise, render the form again so that we can show the users the validation errors.
+        else
+        render :new
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+      end
+    
+      def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        redirect_to employee_path(@employee.id)
+      end
+    
+    private
+
+    def employee_params
+      params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,29 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :first_name, {
+        presence: true
+        }
+    validates :last_name, {
+        presence: true
+        }
+    validates :alias, {
+        presence: true,
+        uniqueness: true
+        }
+    validates :title, {
+        presence: true,
+        uniqueness: true
+        }
+    validates :office, {
+        presence: true
+        }
+    validates :dog_id, {
+        presence: true
+    }
+
+    def name
+        name = self.first_name + ' '+ self.last_name
+        name
+    end
 end
